51759d3ebdc39125abe0ac50e1df12e22edc18fa
hadasa <hadasa@leibish.com>
2025-03-27 20:01:42 +0200

    Product webhook- send notification emails

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,19 +2,26 @@
 import express from 'express';
 import { createOrUpdateHubSpotProduct, deleteHubSpotProduct } from '../lib/hubspot.js';
 import logger from '../utils/logger.js';
+import { sendFailureEmail } from '../lib/email.js';
+
 
 const router = express.Router();
 
 // Webhook for creating/updating a product
 router.post('/product', express.json(), async (req, res) => {
   console.log('🚀 Webhook received for product create/update:');
+  const failures = []; 
 
   try {
-    await createOrUpdateHubSpotProduct(req.body, logger);
+    await createOrUpdateHubSpotProduct(req.body, logger, failures);
     res.status(200).send('Product create/update processed.');
   } catch (error) {
     console.error('❌ Error processing product:', error);
     res.status(500).send('Error processing product webhook.');
+  }
+  // After processing all SKUs, send one email if there are failures
+  if (failures.length > 0) {
+    await sendFailureEmail(failures);
   }
 });
 
