7efb8fe715ccf48cc290ceca36445b09417b5035
hadasa <hadasa@leibish.com>
2025-04-20 17:58:57 +0300

    Refactor static file serving logic for production environment

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ const app = express();
 app.use(express.json());
 
 // Serve static files from the React build folder
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 // Routes
 app.use('/webhooks', webhookRoutes);
